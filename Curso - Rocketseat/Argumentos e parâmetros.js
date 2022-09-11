cbbca864f3416115e88e93b1b547b9d28d1bfc5c
Wilkson Flor Soares <90102289+Wilksonflor@users.noreply.github.com>
2022-09-11 10:39:23 -0300

    Add files via upload

---
+++ Curso - Rocketseat/Argumentos e parâmetros.js
@@ -0,0 +1,11 @@
+
+//Argumentos e parâmetros
+
+// Posso chamar funções dentro de variáveis
+
+
+const sum = function(number1, number2){ // parametros da funções
+console.log(number1+number2)
+}
+
+sum(30,3)
---
+++ Curso - Rocketseat/function.js
@@ -0,0 +1,13 @@
+
+// criar a função
+function criarFrases(){
+  console.log('Estudar é bom')
+  console.log('Paciência e persistência')
+  console.log('Revisão sempre')
+}
+criarFrases()//saber executar a função(ou chamar a função)
+
+//Argumentos e parâmetros
+
+
+
